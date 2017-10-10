efae346bbbb45f06bca207ec57d544ade05f0a74
Drew Lazzeri <dlazzeri1@gmail.com>
2017-10-10 11:17:34 -0700

    I can create and save timers with activities

--- budget.elm
+++ budget.elm
@@ -6,6 +6,7 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Task exposing (perform)
 import Time exposing (..)
+import Dict
 
 main = program
       { init = init
@@ -28,7 +29,7 @@ update msg model=
           in
             if name == "" then model ! []
             else
-              {model | activities = (Activity 0 [] name):: model.activities
+              {model | activities = Dict.insert name (Activity 0 []) model.activities
                      , possibleName = ""
               } ! []
         ActivityTyping str ->
@@ -41,7 +42,14 @@ update msg model=
           case model.live of
             NoTimer -> {model | live = Open time} ! []
             Open start -> {model | live = Closed start time } ![]
-            Closed _ _ -> {model | live = NoTimer} ![]
+            Closed _ _ -> {
+              model | activities = Dict.update activity (ufunc model.live)  model.activities
+                    , live = NoTimer
+              } ! []
+
+ufunc current x = case x of
+  Nothing -> Nothing
+  Just y -> Just <| Activity y.budgeted (current::y.spent)
 
 view model =
     div [ id "topBar" ] <| [
@@ -49,7 +57,7 @@ view model =
       NoTimer -> button [onClick CycleEventTimer ] [text "Start New Timer"]
       Open _ -> button [onClick CycleEventTimer ][text "Stop Timer"]
       Closed _ _ -> div []
-        (List.map .name model.activities
+        (Dict.keys model.activities
           |> List.map (\x -> button [onClick <| GotEventTimer x 0] [text x]))
     , input [ value model.possibleName, onInput ActivityTyping] []
     , button [ onClick NewActivity ] [ text "Add Activity" ]
--- model.elm
+++ model.elm
@@ -2,37 +2,25 @@ module Model exposing (..)
 
 import Maybe exposing (Maybe(..))
 import Time exposing (Time)
+import Dict 
 
 type alias Model =
-  { activities: List Activity
+  { activities: Dict.Dict String Activity
   , live : Entry
   , possibleName : String
   }
 
-
 type alias Activity =
   { budgeted : Int
   , spent : List Entry
-  , name : String }
-
-spent : Activity -> Float
-spent a = List.filterMap duration a.spent |>  List.sum
-
+  }
 
 type Entry = NoTimer | Open Time | Closed Time Time
 
-duration : Entry -> Maybe Time
-duration e =
-  case e of
-    Closed start stop ->  stop - start |> Just
-    Open _ -> Nothing
-    NoTimer -> Nothing
-
-
 init = (
-         { activities= []
-         , live=NoTimer
-         , possibleName=""
+         { activities= Dict.empty
+         , live = NoTimer
+         , possibleName = ""
          }
        , Cmd.none
        )
