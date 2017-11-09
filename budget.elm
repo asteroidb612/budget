54f7f7fde78b4e00ea735eba6487409d60b456a0
Drew Lazzeri <dlazzeri1@gmail.com>
2017-11-08 23:47:49 -0800

    Moved sync to 30 seconds and actions

--- budget.elm
+++ budget.elm
@@ -9,18 +9,24 @@ import Time exposing (..)
 import Dict
 import Tuple
 import Http exposing (send, get, Error(..))
+import Json.Decode
 
 
-main = program
-      { init = (init, fetchActivities)
-      , view = view
-      , update = update
-      , subscriptions = subscriptions
-      }
+main =
+    program
+        { init = ( init, fetchActivities )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  if model.haveSyncedOnce then Time.every (10*Time.second) SendActivities
-                          else Sub.none
+    if model.haveSyncedOnce then
+        Time.every (30 * Time.second) SendActivities
+    else
+        Sub.none
 
 
 type Msg
@@ -33,116 +39,199 @@ type Msg
     | CommitActivities (Result Http.Error (Dict.Dict String Activity))
     | FetchActivities
 
-url =  "https://pebble-timetracking.firebaseio.com/activities.json"
-request = Http.get url decodeActivities
-fetchActivities = Http.send CommitActivities request
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model=
+url =
+    "https://pebble-timetracking.firebaseio.com/activities.json"
+
+
+fetchActivities =
+    let
+        request =
+            Http.get url decodeActivities
+    in
+        Http.send CommitActivities request
+
+
+sendActivities model =
+    let
+        request =
+            Http.request
+                { method = "PUT"
+                , headers = []
+                , url = "https://pebble-timetracking.firebaseio.com/activities.json"
+                , body = Http.jsonBody (encodeActivities model.activities)
+                , expect = Http.expectJson decodeActivities
+                , timeout = Nothing
+                , withCredentials = False
+                }
+    in
+        Http.send CommitActivities request
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
     case msg of
         FetchActivities ->
-           {model | message = "Fetching Activities"} ! [fetchActivities]
+            { model | message = "Fetching Activities" } ! [ fetchActivities ]
 
         CommitActivities (Ok newActivities) ->
-          {model | activities = newActivities
-                 , message = ""
-                 , haveSyncedOnce = True} ! []
+            { model
+                | activities = newActivities
+                , message = ""
+                , haveSyncedOnce = True
+            }
+                ! []
 
-        CommitActivities (Err e) -> case e of
-          Http.Timeout -> {model | message = "timeout"} ! []
-          Http.NetworkError -> {model | message = "network error"} ! []
-          Http.BadUrl x -> {model | message = "badurl: " ++ x }  ! []
-          Http.BadStatus x ->  {model | message = "badstatus: " ++ (toString x) } ! []
-          Http.BadPayload x y -> {model | message = "badpayload: " ++ x ++ "\n" ++ (toString y)} ! []
+        CommitActivities (Err e) ->
+            case e of
+                Http.Timeout ->
+                    { model | message = "timeout" } ! []
+
+                Http.NetworkError ->
+                    { model | message = "network error" } ! []
+
+                Http.BadUrl x ->
+                    { model | message = "badurl:\n" ++ x } ! []
+
+                Http.BadStatus x ->
+                    { model | message = "badstatus:\n" ++ (toString x) } ! []
+
+                Http.BadPayload x y ->
+                    { model | message = "badpayload:\n" ++ x ++ "\n" ++ (toString y) } ! []
 
         SendActivities _ ->
-          let request = Http.request { method = "PUT"
-                        , headers = []
-                        , url = "https://pebble-timetracking.firebaseio.com/activities.json"
-                        , body = Http.jsonBody (encodeActivities model.activities)
-                        , expect = Http.expectJson decodeActivities
-                        , timeout = Nothing
-                        , withCredentials = False
-                        }
-          in {model | message="Sending Activities"} ! [Http.send CommitActivities request]
+            { model | message = "Sending Activities" } ! [ sendActivities model ]
 
         NewActivity ->
-          let name = model.possibleName
-          in
-            if name == "" then model ! []
-            else
-              {model | activities = Dict.insert name (Activity 0 []) model.activities
-                     , possibleName = ""
-                     } ! []
+            let
+                name =
+                    model.possibleName
+
+                new_model =
+                    { model
+                        | activities = Dict.insert name (Activity 0 []) model.activities
+                        , possibleName = ""
+                    }
+            in
+                if name == "" then
+                    model ! []
+                else
+                    new_model ! [ sendActivities new_model ]
+
         ActivityTyping str ->
-          {model | possibleName = str
-                 , message = "Unsynced Changes"} ! []
+            { model
+                | possibleName = str
+            }
+                ! []
 
         CycleEventTimer ->
-          model ! [Task.perform (GotEventTimer "") now]
+            model ! [ Task.perform (GotEventTimer "") now ]
 
         GotEventTimer label time ->
-
             case model.live of
-              NoTimer -> {model | live = Open time} ! []
-              Open start -> {model | live = Closed start time } ![]
-              Closed start stop ->
-                let
-                    updateSpent x = case x of
-                      Nothing -> Nothing
-                      Just y -> Just <| Activity y.budgeted ((start, stop)::y.spent)
-                in
-                  {model | activities = Dict.update label updateSpent model.activities
-                        , live = NoTimer
-                  } ! []
-        Budget label unparsedAmount->
+                NoTimer ->
+                    { model | live = Open time } ! []
+
+                Open start ->
+                    { model | live = Closed start time } ! []
+
+                Closed start stop ->
+                    let
+                        updateSpent x =
+                            case x of
+                                Nothing ->
+                                    Nothing
+
+                                Just y ->
+                                    Just <| Activity y.budgeted (( start, stop ) :: y.spent)
+
+                        new_model =
+                            { model
+                                | activities = Dict.update label updateSpent model.activities
+                                , live = NoTimer
+                            }
+                    in
+                        new_model ! [ sendActivities new_model ]
+
+        Budget label unparsedAmount ->
             let
-              amount = String.toInt unparsedAmount|> Result.toMaybe |> Maybe.withDefault 0
-              updateBudget x = case x of
-                Nothing -> Nothing
-                Just y -> Just <| Activity amount y.spent
+                amount =
+                    String.toInt unparsedAmount |> Result.toMaybe |> Maybe.withDefault 0
+
+                updateBudget x =
+                    case x of
+                        Nothing ->
+                            Nothing
+
+                        Just y ->
+                            Just <| Activity amount y.spent
+
+                new_model =
+                    { model
+                        | activities = Dict.update label updateBudget model.activities
+                        , message = "Unsynced Changes"
+                    }
             in
-              {model | activities = Dict.update label updateBudget model.activities
-                     , message = "Unsynced Changes"} ! []
+                new_model ! [ sendActivities new_model ]
+
 
 view model =
     div [ id "topBar" ]
-    [ div []
-      [ div [] <| case model.live of
-        NoTimer ->
-          [ button [onClick CycleEventTimer ] [text "Start New Timer"]]
-        Open time ->
-          [ button [onClick CycleEventTimer ] [text "Stop Timer"]
-          , text (toString time)]
-        Closed start stop ->
-          [(stop - start)
-            |> inMinutes
-            |> toString
-            |> \x -> x ++ " minutes"
-            |> text
-          , span [] <| (Dict.keys model.activities
-              |> List.map (\x -> button [onClick <| GotEventTimer x 0] [text x]))
-          ]
-      ]
-    , div [id "list"] (Dict.toList model.activities |> List.map activityRow )
-    , input [ value model.possibleName, onInput ActivityTyping] []
-    , button [ onClick NewActivity ] [ text "Add Activity" ]
-    , div []
-      [ button [onClick (SendActivities 0)] [ text "Send Activities"]
-      , button [onClick FetchActivities] [ text "Fetch Activities"]
-      ]
-    , div [] [text model.message]
-    ]
+        [ div []
+            [ div [] <|
+                case model.live of
+                    NoTimer ->
+                        [ button [ onClick CycleEventTimer ] [ text "Start New Timer" ] ]
+
+                    Open time ->
+                        [ button [ onClick CycleEventTimer ] [ text "Stop Timer" ]
+                        , text (toString time)
+                        ]
+
+                    Closed start stop ->
+                        [ (stop - start)
+                            |> inMinutes
+                            |> toString
+                            |> \x ->
+                                x
+                                    ++ " minutes"
+                                    |> text
+                        , span [] <|
+                            (Dict.keys model.activities
+                                |> List.map (\x -> button [ onClick <| GotEventTimer x 0 ] [ text x ])
+                            )
+                        ]
+            ]
+        , div [ id "list" ] (Dict.toList model.activities |> List.map activityRow)
+        , input [ value model.possibleName, onInput ActivityTyping ] []
+        , button [ onClick NewActivity ] [ text "Add Activity" ]
+        , div []
+            [ button [ onClick (SendActivities 0) ] [ text "Send Activities" ]
+            , button [ onClick FetchActivities ] [ text "Fetch Activities" ]
+            ]
+        , div [] [ text model.message ]
+        ]
+
+
+onBlurWithTargetValue : (String -> msg) -> Attribute msg
+onBlurWithTargetValue tagger =
+    on "blur" (Json.Decode.map tagger targetValue)
+
 
 activityRow x =
-  let
-    activityLabel = Tuple.first x
-    activity = Tuple.second x
-  in
-    div []
-      [ text activityLabel
-      , input [ value <| toString activity.budgeted
-              , onInput <| Budget activityLabel
-              ] []
-      , text (toString (List.sum(List.map duration activity.spent)))
-      ]
+    let
+        activityLabel =
+            Tuple.first x
+
+        activity =
+            Tuple.second x
+    in
+        div []
+            [ text activityLabel
+            , input
+                [ value <| toString activity.budgeted
+                , onBlurWithTargetValue <| Budget activityLabel
+                ]
+                []
+            , text (toString (List.sum (List.map duration activity.spent)))
+            ]
--- model.elm
+++ model.elm
@@ -6,45 +6,77 @@ import Dict
 import Json.Decode as Decode
 import Json.Encode as Encode
 
+
 type alias Model =
-  { activities: Dict.Dict String Activity
-  , live : Timer
-  , possibleName : String
-  , message : String
-  , haveSyncedOnce : Bool
-  }
+    { activities : Dict.Dict String Activity
+    , live : Timer
+    , possibleName : String
+    , message : String
+    , haveSyncedOnce : Bool
+    }
+
 
 type alias Activity =
-  { budgeted : Int
-  , spent : List Entry
-  }
+    { budgeted : Int
+    , spent : List Entry
+    }
 
-type alias Entry = (Time, Time)
-type Timer = NoTimer | Open Time | Closed Time Time
+
+type alias Entry =
+    ( Time, Time )
+
+
+type Timer
+    = NoTimer
+    | Open Time
+    | Closed Time Time
+
 
 duration : Entry -> Time
-duration (start, stop) = stop - start |> inMinutes
+duration ( start, stop ) =
+    stop - start |> inMinutes
 
-init =   { activities= Dict.empty
-         , live = NoTimer
-         , possibleName = ""
-         , message = ""
-         , haveSyncedOnce = False
-         }
 
-decodeEntry = Decode.map2 (,) (Decode.field "start" Decode.float) (Decode.field "stop" Decode.float)
-decodeActivity = Decode.map2 Activity (Decode.field "budgeted" Decode.int)
-                                      (Decode.andThen decodeSpent (Decode.maybe (Decode.field "spent" (Decode.list decodeEntry))))
-decodeSpent spent = Decode.succeed <| withDefault [] spent
+init =
+    { activities = Dict.empty
+    , live = NoTimer
+    , possibleName = ""
+    , message = ""
+    , haveSyncedOnce = False
+    }
 
-decodeActivities = Decode.dict decodeActivity
+
+decodeEntry =
+    Decode.map2 (,) (Decode.field "start" Decode.float) (Decode.field "stop" Decode.float)
+
+
+decodeActivity =
+    Decode.map2 Activity
+        (Decode.field "budgeted" Decode.int)
+        (Decode.andThen decodeSpent (Decode.maybe (Decode.field "spent" (Decode.list decodeEntry))))
+
+
+decodeSpent spent =
+    Decode.succeed <| withDefault [] spent
+
+
+decodeActivities =
+    Decode.dict decodeActivity
+
 
 encodeEntry : Entry -> Encode.Value
-encodeEntry (start, stop) = Encode.object [("start", Encode.float start), ("stop", Encode.float stop)]
+encodeEntry ( start, stop ) =
+    Encode.object [ ( "start", Encode.float start ), ( "stop", Encode.float stop ) ]
 
-encodeActivity a = Encode.object [("budgeted", Encode.int a.budgeted)
-                                , ("spent", Encode.list (List.map encodeEntry a.spent))]
 
-encodeActivities a = Dict.toList a
-                        |> List.map (\(k,v) -> (k, encodeActivity v))
-                        |> Encode.object
+encodeActivity a =
+    Encode.object
+        [ ( "budgeted", Encode.int a.budgeted )
+        , ( "spent", Encode.list (List.map encodeEntry a.spent) )
+        ]
+
+
+encodeActivities a =
+    Dict.toList a
+        |> List.map (\( k, v ) -> ( k, encodeActivity v ))
+        |> Encode.object
